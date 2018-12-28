80965c7c678ba3ed9209471ea0122827553faec7
Rachel Chen <rachelchen0418@gmail.com>
2018-12-28 13:11:33 -0500

    Initial Commit

---
+++ EasyTips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyTips
+//
+//  Created by Rachel Chen on 12/28/18.
+//  Copyright © 2018 Rachel Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
